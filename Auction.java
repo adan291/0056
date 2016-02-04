bcea608a205674bf6a4db8aa5980bf5d7bcf1053
adan291 <adanx05@gmail.com>
2016-02-04 12:47:44 +0100

    Añadido metodo para mostrar los lotes que no se han pujado

---
+++ Auction.java
@@ -0,0 +1,166 @@
+import java.util.ArrayList;
+
+/**
+ * A simple model of an auction.
+ * The auction maintains a list of lots of arbitrary length.
+ *
+ * @author David J. Barnes and Michael KÃ¶lling.
+ * @version 2011.07.31
+ */
+public class Auction
+{
+    // The list of Lots in this auction.
+    private ArrayList<Lot> lots;
+    // The number that will be given to the next lot entered
+    // into this auction.
+    private int nextLotNumber;
+
+    /**
+     * Create a new auction.
+     */
+    public Auction()
+    {
+        lots = new ArrayList<Lot>();
+        nextLotNumber = 1;
+    }
+
+    /**
+     * Enter a new lot into the auction.
+     * @param description A description of the lot.
+     */
+    public void enterLot(String description)
+    {
+        lots.add(new Lot(nextLotNumber, description));
+        nextLotNumber++;
+    }
+
+    /**
+     * Show the full list of lots in this auction.
+     */
+    public void showLots()
+    {
+        for(Lot lot : lots) {
+            System.out.println(lot.toString());
+        }
+    }
+
+    /**
+     * Make a bid for a lot.
+     * A message is printed indicating whether the bid is
+     * successful or not.
+     * 
+     * @param lotNumber The lot being bid for.
+     * @param bidder The person bidding for the lot.
+     * @param value  The value of the bid.
+     */
+    public void makeABid(int lotNumber, Person bidder, long value)
+    {
+        Lot selectedLot = getLot(lotNumber);
+        if(selectedLot != null) {
+
+            boolean successful = selectedLot.bidFor(new Bid(bidder, value));
+            if(successful) {
+                System.out.println("The bid for lot number " +
+                    lotNumber + " was successful.");
+            }
+            else {
+                // Report which bid is higher.
+
+                System.out.println("Lot number: " + lotNumber +
+                    " already has a bid of: " +
+                    selectedLot.getHighestBid().getValue());
+            }
+        }
+    }
+
+    /**
+     * Return the lot with the given number. Return null
+     * if a lot with this number does not exist.
+     * @param lotNumber The number of the lot to return.
+     */
+    public Lot getLot(int lotNumber)
+    {
+        if((lotNumber >= 1) && (lotNumber < nextLotNumber)) {
+            // The number seems to be reasonable.
+            Lot selectedLot = lots.get(lotNumber - 1);
+            // Include a confidence check to be sure we have the
+            // right lot.
+            if(selectedLot.getNumber() != lotNumber) {
+                System.out.println("Internal error: Lot number " +
+                    selectedLot.getNumber() +
+                    " was returned instead of " +
+                    lotNumber);
+                // Don't return an invalid lot.
+                selectedLot = null;
+            }
+            return selectedLot;
+        }
+        else {
+            System.out.println("Lot number: " + lotNumber +
+                " does not exist.");
+            return null;
+        }
+    }
+
+    /**
+     * Metodo que muestra por pantalla los detalles de todos los items que se estén subastando actualmente
+     */
+
+    public void close()
+    {
+        for(Lot elemento : lots){
+            System.out.println(elemento.toString());
+            Bid pujaMasAltaParaEsteLot = elemento.getHighestBid();
+            if( pujaMasAltaParaEsteLot == null){
+                System.out.println("No hay pujas de momento para el item");
+            }
+            else{
+                System.out.println("Quien ha pujado mas alto a sido: " +  pujaMasAltaParaEsteLot.getBidder().getName());
+                System.out.println("Y realizo una puja de: " + pujaMasAltaParaEsteLot.getValue() );
+            }
+        }
+    }
+
+    /**
+     * Prueba de mostrar
+     */
+
+    public void getUnsold2()
+    {
+      //  for(Lot elemento : lots){
+
+       //     Bid pujaMasAltaParaEsteLot = elemento.getHighestBid();
+         //   if( pujaMasAltaParaEsteLot == null){
+           //     System.out.println("No ha habido ninguna puja para los lotes: " +  pujaMasAltaParaEsteLot.getBidder().getNumber());
+            //}
+
+        }
+ 
+
+    /**
+     * Metodo que devuelva una colección de todos los items por los que no habido ninguna puja en este momento
+     */
+    public ArrayList getUnsold() {
+        ArrayList noLote = new ArrayList<Lot>();        
+        for (Lot lot : lots){
+            if (lot.getHighestBid() == null){
+                noLote.add(lot);
+            }
+        }        
+        return noLote;
+    }
+    
+        /**
+         * Metodo que crea una lista con los lotes por los que no se ha pujado.
+         */
+        public ArrayList getUnsold() {
+            ArrayList noLote = new ArrayList<Lot>();        
+            for (Lot lot : lots){
+                if (lot.getHighestBid() == null){
+                    noLote.add(lot);
+                }
+            }        
+            return noLote;
+        }
+    }
+
